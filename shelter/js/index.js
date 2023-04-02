3cfe67a777cec0fa10a95e05f7140c50f1ca890f
shchegolenkov <denis.amorales@ya.ru>
2023-04-02 22:26:47 +0300

    feat: mobile menu layout

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -1,3 +1,13 @@
-console.log(
-  "Самооценка 100 баллов из 100:\n1. Вёрстка страницы Main соответствует макету при ширине экрана 1280px: +14\n2. Вёрстка страницы Main соответствует макету при ширине экрана 768px: +14\n3. Вёрстка страницы Main соответствует макету при ширине экрана 320px: +14\n4. Вёрстка страницы Pets соответствует макету при ширине экрана 1280px: +6\n5. Вёрстка страницы Pets соответствует макету при ширине экрана 768px: +6\n6. Вёрстка страницы Pets соответствует макету при ширине экрана 320px: +6\n7. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки, справа от отдельных блоков не появляются белые поля. Весь контент страницы при этом сохраняется: не обрезается и не удаляется: +20\n8. Верстка резиновая: при плавном изменении размера экрана от 1280px до 320px верстка подстраивается под этот размер, элементы верстки меняют свои размеры и расположение, не наезжают друг на друга, изображения могут менять размер, но сохраняют правильные пропорции: +8\n9. При ширине экрана меньше 768px на обеих страницах меню в хедере скрывается, появляется иконка бургер-меню: +4\n10. Верстка обеих страниц валидная: +8"
-);
+// Mobile menu
+
+const mobileMenuButton = document.querySelector(".mobile-menu-icon");
+const mobileMenu = document.querySelector(".mobile-menu");
+const backgroundOverlay = document.querySelector(".background-overlay");
+
+mobileMenuButton.addEventListener("click", mobileMenuToggle);
+
+function mobileMenuToggle() {
+  mobileMenuButton.classList.toggle("mobile-menu-icon--active");
+  mobileMenu.classList.toggle("mobile-menu--active");
+  backgroundOverlay.classList.toggle("background-overlay--active");
+}

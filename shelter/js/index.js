5566f77e137648e3a189c783494316c771d2f819
shchegolenkov <denis.amorales@ya.ru>
2023-04-03 19:03:55 +0300

    feat: mobile menu actions

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -2,6 +2,7 @@
 
 const mobileMenuButton = document.querySelector(".mobile-menu-icon");
 const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuLinkList = document.querySelector(".header-nav__list");
 const backgroundOverlay = document.querySelector(".background-overlay");
 
 mobileMenuButton.addEventListener("click", mobileMenuToggle);
@@ -10,4 +11,26 @@ function mobileMenuToggle() {
   mobileMenuButton.classList.toggle("mobile-menu-icon--active");
   mobileMenu.classList.toggle("mobile-menu--active");
   backgroundOverlay.classList.toggle("background-overlay--active");
+  document.body.classList.toggle("body--scroll-disabled");
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  // Close menu when click the link:
+  mobileMenuLinkList.addEventListener("click", function (e) {
+    if (e.target.classList.contains("header-nav__link")) {
+      mobileMenuToggle();
+    }
+  });
 }
+
+window.addEventListener("keydown", function (e) {
+  // Close menu by escape button:
+  if (
+    mobileMenu.classList.contains("mobile-menu--active") &&
+    e.key === "Escape"
+  )
+    mobileMenuToggle();
+});
+
+window.addEventListener("click", function (e) {
+  console.log(e.target.classList);
+});
